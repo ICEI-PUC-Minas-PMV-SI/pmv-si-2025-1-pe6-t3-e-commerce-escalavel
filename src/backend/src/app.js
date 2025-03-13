04d1d1f38aa4c844589a097f5f4c46fedea76306
Petronio <petroniomoreira1@hotmail.com>
2025-03-13 16:59:51 -0400

    servidor local config ajsutes codigo

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,5 +1,8 @@
-require ('dotenv').config;
+require ('dotenv').config();
 const express = require('express');
 const app = express();
+
+// Middleware para tratar JSON
+app.use(express.json());
 
 app.listen(process.env.PORT, ()=> {console.log("Servidor rodando...")})
