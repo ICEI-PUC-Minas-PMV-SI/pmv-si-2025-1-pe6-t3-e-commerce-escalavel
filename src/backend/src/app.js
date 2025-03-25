061459403669c6cd34e9e305ff538582932bc3e9
SeuNomeDeUsuário <seuemail@example.com>
2025-03-25 18:48:24 -0300

    migração de repositório

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,8 +1,20 @@
-require ('dotenv').config();
-const express = require('express');
-const app = express();
+import express from 'express';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+import vendaRoutes from './routes/vendaRoutes.js';
+import 'dotenv/config';
 
-// Middleware para tratar JSON
+const app = express();
 app.use(express.json());
 
-app.listen(process.env.PORT, ()=> {console.log("Servidor rodando...")})
+// Rotas
+app.use('/api', userRoutes); // Rotas de usuários
+app.use('/api', productRoutes); // Rotas de produtos
+app.use('/api', cartRoutes); // Rotas do carrinho
+app.use('/api', vendaRoutes);
+
+// Iniciar servidor
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
