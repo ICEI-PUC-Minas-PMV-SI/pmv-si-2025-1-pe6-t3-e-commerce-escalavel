4b7ecc9b3821849341264b5333c56cb3572fcd9a
SeuNomeDeUsuário <seuemail@example.com>
2025-03-25 19:48:47 -0300

    alteração de diretório

--- src/backend/src/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-export default {
-    jwtSecret: 'sua_chave_secreta_super_segura', // Use uma chave segura e única
-    jwtExpiration: '10h', // Tempo de expiração do token (ex: 1 hora)
-  };
