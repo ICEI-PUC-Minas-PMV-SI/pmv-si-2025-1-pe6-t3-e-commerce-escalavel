e2f01f7ed0886ece911d1776e0fe5098a21fd3f7
SeuNomeDeUsuário <seuemail@example.com>
2025-04-07 21:42:56 -0300

    buscar todas as as avaliações

--- src/routes/avaliacaoRoutes.js
+++ src/routes/avaliacaoRoutes.js
@@ -1,11 +1,16 @@
-// routes/avaliacaoRoutes.js
 import express from 'express';
-import { criarAvaliacao, listarAvaliacoes } from '../controllers/avaliacaoController.js';
+import {
+  criarAvaliacao,
+  listarAvaliacoes,
+  listarTodasAvaliacoes
+} from '../controllers/avaliacaoController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+router.get('/avaliacoes', listarTodasAvaliacoes); // <- nova rota
 router.post('/avaliacoes', authMiddleware, criarAvaliacao);
 router.get('/avaliacoes/:produtoId', listarAvaliacoes);
+
 
 export default router;
