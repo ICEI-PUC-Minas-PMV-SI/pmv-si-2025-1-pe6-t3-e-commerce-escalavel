cd2e0e2c5247910265a235a84014847110787351
SeuNomeDeUsuário <seuemail@example.com>
2025-05-23 19:24:40 -0300

    teste vite e vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'frontend', 
+  root: 'frontend', // pasta onde está o index.html
   build: {
-    outDir: 'dist', 
+    outDir: 'dist', // saída dentro da raiz do projeto
     emptyOutDir: true,
   },
 });
