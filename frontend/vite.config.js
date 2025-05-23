a1236b5e7546961519ed1408f282468781367c32
SeuNomeDeUsuário <seuemail@example.com>
2025-05-23 19:02:18 -0300

    teste vercel e render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.', // ou remova completamente essa linha se estiver na raiz do frontend
+  root: "frontend", 
   build: {
     outDir: 'dist',
   },
