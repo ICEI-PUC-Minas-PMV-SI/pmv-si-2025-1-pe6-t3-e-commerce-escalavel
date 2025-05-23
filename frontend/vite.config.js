50c2e58027fb3b77f5fc2411cdd65f2ba6111606
SeuNomeDeUsuário <seuemail@example.com>
2025-05-23 19:10:25 -0300

    teste vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "frontend", 
+  root: 'frontend', 
   build: {
-    outDir: 'dist',
+    outDir: '../dist', 
+    emptyOutDir: true,
   },
-  publicDir: 'public'
-})
+});
