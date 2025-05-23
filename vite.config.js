c9c895dcc0ed37f3e576efe4a83142c59f91dc54
SeuNomeDeUsuário <seuemail@example.com>
2025-05-23 19:32:27 -0300

    teste vercel

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'frontend', // pasta onde está o index.html
-  build: {
-    outDir: 'dist', // saída dentro da raiz do projeto
-    emptyOutDir: true,
-  },
-});
